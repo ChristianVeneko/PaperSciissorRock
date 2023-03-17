17947f2db63e293bed054e638dc6735b5cb560ed
Christian Mendoza <christianmendozater@gmail.com>
2023-03-16 23:06:18 -0400

    Listo ;)

--- main.js
+++ main.js
@@ -1,22 +1,88 @@
-const rock = document.getElementById('rock')
-const paper = document.getElementById('paper')
-const scissors = document.getElementById('scissors')
+const resultDiv = document.getElementById("result");
+const rock = document.getElementById("rock");
+const paper = document.getElementById("paper");
+const scissors = document.getElementById("scissors");
 
-function game(choice){
-
+function cpuChoice() {
+  const choices = ["rock", "paper", "scissors"];
+  const randomIndex = Math.floor(Math.random() * 3);
+  return choices[randomIndex];
 }
 
-function main(){
-  rock.addEventListener('click', function(){
-    console.log('rock')
-  })
-  paper.addEventListener('click', function(){
-    console.log('paper')
-  })
+function showResult(cpuChoice, result) {
+  resultDiv.innerHTML = "";
+  
+  const resultText = document.createElement("h1");
+  const newText = document.createTextNode(result);
+  resultText.appendChild(newText);
+  resultDiv.appendChild(resultText);
 
-  scissors.addEventListener('click', function(){
-    console.log('scissors')
-  })
+  const cpuChoiceImg = document.createElement('img');
+  
+  cpuChoiceImg.classList.add('icon'); 
+
+  let images = ["./images/Paper.jpeg", "./images/Rock.jpeg", './images/Scissor.jpeg']
+  if(cpuChoice == 'rock'){
+    cpuChoiceImg.src = images[1];
+  }else if(cpuChoice == 'paper'){
+    cpuChoiceImg.src = images[0]
+  }else{
+    cpuChoiceImg.src = images[2]
+  }
+  
+  resultDiv.appendChild(cpuChoiceImg);
 }
 
-main()
+function game(choice) {
+  let result = "";
+  const cpu = cpuChoice();
+  const resultText = choice + cpu;
+  console.log(resultText);
+
+  switch (resultText) {
+    case "rockscissors":
+      result = "win";
+      break;
+    case "rockpaper":
+      result = "lose";
+      break;
+    case "rockrock":
+      result = "tie";
+      break;
+    case "paperrock":
+      result = "win";
+      break;
+    case "paperscissors":
+      result = "lose";
+      break;
+    case "paperpaper":
+      result = "tie";
+      break;
+    case "scissorspaper":
+      result = "win";
+      break;
+    case "scissorsrock":
+      result = "lose";
+      break;
+    case "scissorsscissors":
+      result = "tie";
+      break;
+  }
+
+  showResult(cpu, result);
+}
+
+function main() {
+  rock.addEventListener("click", function () {
+    game("rock");
+  });
+  paper.addEventListener("click", function () {
+    game("paper");
+  });
+
+  scissors.addEventListener("click", function () {
+    game("scissors");
+  });
+}
+
+main();
